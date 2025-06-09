73f5fc7052cde4c303bae6b3baee9d0029d3d9d4
Amith0205 <89029802+Amith0205@users.noreply.github.com>
2025-06-09 23:45:20 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-require("dotenv").config()
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
