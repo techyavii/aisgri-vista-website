f7d99a5420b6ea21da386573933b36c88f14f478
aviral <jain24082004@gmail.com>
2025-11-14 21:58:21 +0530

    resolving download problem

--- src/pages/Downloads.tsx
+++ src/pages/Downloads.tsx
@@ -8,22 +8,22 @@ const Downloads = () => {
   const downloadItems = [
     {
       name: 'Checklist',
-      link: 'https://res.cloudinary.com/da0yxppx2/image/upload/v1748602693/DASGRI%20adroid%20connectz/zfbh444d95upzrdjzym3.pdf',
+      link: '/Checklist.pdf',
       icon: <FileText className="mr-2" size={18} />
     },
     {
       name: 'Manuscript guidelines',
-      link: 'https://res.cloudinary.com/da0yxppx2/image/upload/v1748602694/DASGRI%20adroid%20connectz/a53rrfp19k1xgj97rrxb.pdf',
+      link: '/ManuscriptGuidelines.pdf',
       icon: <FileText className="mr-2" size={18} />
     },
     {
       name: 'Springer Word Template',
-      link: 'https://resource-cms.springernature.com/springer-cms/rest/v1/content/19238648/data/v1',
+      link: '/Springer Word Template (1)_2025-11-14_21_46.zip',
       icon: <FileText className="mr-2" size={18} />
     },
     {
       name: 'Springer Latex Template',
-      link: 'https://resource-cms.springernature.com/springer-cms/rest/v1/content/19238706/data/v1',
+      link: '/Springer LaTeX Template (1)_2025-11-14_21_47.zip',
       icon: <FileText className="mr-2" size={18} />
     },
   ];
